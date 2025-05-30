f0610ab2f4654dc99f84026a7a9bd82ea6264255
Enrico Ardian <123723468+EnricoArdian@users.noreply.github.com>
2025-05-30 13:33:41 +0700

    Create calculate_triangle_area.cpp

---
+++ C++/calculate_triangle_area.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ int Luas;
+ int alas;
+ int tinggi;
+ cout << "Masukkan alas ";
+ cin >> alas;
+ cout << "Masukkan tinggi ";
+ cin >> tinggi;
+ Luas = alas * tinggi / 2;
+ cout << "luasSegitiga" << Luas << endl;
+}
