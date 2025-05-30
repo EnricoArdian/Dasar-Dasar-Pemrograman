2fb08efebe6c5c19f3303dea5540d27b937d6dc6
Enrico Ardian <123723468+EnricoArdian@users.noreply.github.com>
2025-05-30 13:28:07 +0700

    Create half_circle_area.cpp

---
+++ C++/half_circle_area.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+ float Luas;
+ float phi = 3.14;
+ int radian;
+ cout << "Masukkan phi ";
+ cin >> phi;
+ cout << "Masukkan radian ";
+ cin >> radian;
+ Luas = 3.14 * radian * radian / 2;
+ cout << "Luas Setengah Lingkaran" << Luas << endl;
+}
